4cd85f7dbe225643947a09d9eb44987377722ee5
Alexotieno1717 <alexotieno900@gmail.com>
2025-11-05 16:17:46 +0300

    [feat]:building a post model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Profile extends Model
@@ -22,5 +23,15 @@ class Profile extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function posts(): hasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function topLevelPost(): hasMany
+    {
+        return $this->hasMany(Post::class)->whereNull('parent_id');
     }
 }
