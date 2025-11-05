00ecc9544da7c2505e266b6c7b1e28df3bbcdd92
Alexotieno1717 <alexotieno900@gmail.com>
2025-11-05 16:01:47 +0300

    [feat]: created a Post model,migration and factiory

---
+++ app/Models/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Profile extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'display_name',
+        'handle',
+        'avatar_url',
+        'bio'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
