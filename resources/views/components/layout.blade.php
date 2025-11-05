e19b245306925160e57dbc84a9be6820a6957f6f
Alexotieno1717 <alexotieno900@gmail.com>
2025-11-05 13:12:51 +0300

    [feat]: created artist to follow component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="color-scheme" content="dark" />
-    <title>pixel</title>
+    <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-pixel-dark text-pixel-light flex gap-16 px-4 h-dvh overflow-clip">
--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -9,74 +9,7 @@
     />
     <hr class="border-pixel-light/10 mt-2" />
     <!-- Artists to follow -->
-    <div class="border-pixel-light/10 mt-10 border p-4">
-        <h2 class="text-pixel-light/60 text-sm">Artists to follow</h2>
-        <ol class="mt-4 flex flex-col gap-4">
-            <li class="flex items-center justify-between gap-4">
-                <div class="flex items-center gap-2.5">
-                    <img
-                        src="/images/alessia.png"
-                        alt="Avatar of Alessia"
-                        class="size-8 object-cover"
-                    />
-                    <p class="truncate text-sm">alessia_draws</p>
-                </div>
-                <button
-                    class="bg-pixel-dark/50 hover:bg-pixel-dark/60 active:bg-pixel-dark/75 border-pixel/50 hover:border-pixel/60 active:border-pixel/75 text-pixel border px-2 py-1 text-sm"
-                >
-                    Follow
-                </button>
-            </li>
-            <li class="flex items-center justify-between gap-4">
-                <div class="flex items-center gap-2.5">
-                    <img
-                        src="/images/anne.png"
-                        alt="Avatar of Anne"
-                        class="size-8 object-cover"
-                    />
-                    <p class="truncate text-sm">just_anne</p>
-                </div>
-                <button
-                    class="bg-pixel-dark/50 hover:bg-pixel-dark/60 active:bg-pixel-dark/75 border-pixel/50 hover:border-pixel/60 active:border-pixel/75 text-pixel border px-2 py-1 text-sm"
-                >
-                    Follow
-                </button>
-            </li>
-            <li class="flex items-center justify-between gap-4">
-                <div class="flex items-center gap-2.5">
-                    <img
-                        src="/images/mr-anderson.png"
-                        alt="Avatar of Mr. Anderson"
-                        class="size-8 object-cover"
-                    />
-                    <p class="truncate text-sm">Mr. Anderson</p>
-                </div>
-                <button
-                    class="bg-pixel-dark/50 hover:bg-pixel-dark/60 active:bg-pixel-dark/75 border-pixel/50 hover:border-pixel/60 active:border-pixel/75 text-pixel border px-2 py-1 text-sm"
-                >
-                    Follow
-                </button>
-            </li>
-            <li class="flex items-center justify-between gap-4">
-                <div class="flex items-center gap-2.5">
-                    <img
-                        src="/images/michael.png"
-                        alt="Avatar of Michael"
-                        class="size-8 object-cover"
-                    />
-                    <p class="truncate text-sm">Michael</p>
-                </div>
-                <button
-                    class="bg-pixel-dark/50 hover:bg-pixel-dark/60 active:bg-pixel-dark/75 border-pixel/50 hover:border-pixel/60 active:border-pixel/75 text-pixel border px-2 py-1 text-sm"
-                >
-                    Follow
-                </button>
-            </li>
-        </ol>
-        <a href="#" class="text-pixel-light/60 mt-4 inline-block text-sm"
-        >Show more</a
-        >
-    </div>
+    <x-artist-to-follow />
 
     <!-- Follow idea streams -->
     <div class="border-pixel-light/10 mt-4 border p-4">
