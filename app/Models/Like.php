25c6e5296d38b53ddb4770fadb7bebf289e6acf0
Alexotieno1717 <alexotieno900@gmail.com>
2025-11-06 12:28:23 +0300

    [feat]: created test for liking a post

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -16,6 +16,21 @@ class Like extends Model
         'post_id',
     ];
 
+    public static function createLike(Profile $profile, Post $post)
+    {
+        return static::firstOrCreate([
+            'profile_id' => $profile->id,
+            'post_id' => $post->id
+        ]);
+    }
+
+    public static function removeLike(Profile $profile, Post $post): bool
+    {
+        return static::where('profile_id', $profile->id)
+            ->where('post_id', $post->id)
+            ->delete() > 0;
+    }
+
     public function profile():BelongsTo
     {
         return $this->belongsTo(Profile::class);
