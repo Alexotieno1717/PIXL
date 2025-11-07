c3dc19754e18e814efafe0bc3efcb50389ab096e
Alexotieno1717 <alexotieno900@gmail.com>
2025-11-07 12:27:07 +0300

    [feat]: created the header component view

--- resources/views/components/reply.blade.php
+++ resources/views/components/reply.blade.php
@@ -4,7 +4,7 @@
         aria-hidden="true"
         class="bg-pixel-light/10 absolute top-0 left-5 h-full w-px group-last/li:h-4"
     ></div>
-    <a href="/profile" class="isolate shrink-0">
+    <a href="{{ route('profile.show', $post->profile) }}" class="isolate shrink-0">
         <img
             src="{{ $post->profile->avatar_url }}"
             alt="Avatar for {{ $post->profile->display_name }}"
@@ -15,13 +15,13 @@
         <div class="flex items-center justify-between gap-4">
             <div class="flex items-center gap-2.5">
                 <p>
-                    <a class="hover:underline" href="/profile">{{ $post->profile->display_name }}</a>
+                    <a class="hover:underline" href="{{ route('profile.show', $post->profile) }}">{{ $post->profile->display_name }}</a>
                 </p>
                 <p class="text-pixel-light/40 text-xs">{{ $post->created_at }}</p>
                 <p>
                     <a
                         class="text-pixel-light/40 hover:text-pixel-light/60 text-xs"
-                        href="/profile"
+                        href="{{ route('profile.show', $post->profile) }}"
                     >{{ $post->profile->handle }}</a
                     >
                 </p>
@@ -44,6 +44,7 @@
         <div class="mt-4 flex flex-col gap-3 text-sm">
             {!! $post->content !!}
         </div>
+        @if($showEngagement)
         <!-- Action buttons -->
         <div class="mt-6 flex items-center justify-between gap-4">
             <div class="flex items-center gap-8">
@@ -271,5 +272,6 @@
                 </div>
             </div>
         </div>
+        @endif
     </div>
 </li>
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -22,74 +22,14 @@
             </svg>
             <span>back</span>
         </a>
-        <!-- Profile header -->
-        <header>
-            <img src="{{ $profile->cover_url }}" alt="" />
-            <div
-                class="-mt-10 flex flex-wrap items-end justify-between gap-4 md:-mt-16"
-            >
-                <div class="flex items-end gap-4">
-                    <img
-                        src="{{ $profile->avatar_url }}"
-                        alt="Avatar for {{ $profile->display_name }}"
-                        class="size-20 object-cover md:size-32"
-                    />
-                    <div class="flex flex-col text-sm md:gap-1">
-                        <p class="text-lg md:text-xl">{{ $profile->display_name }}</p>
-                        <p class="text-pixel-light/60 text-sm">{{ "@{$profile->handle}" }}</p>
-                    </div>
-                </div>
-                <a
-                    href="#"
-                    class="bg-pixel-dark/50 hover:bg-pixel-dark/60 active:bg-pixel-dark/75 border-pixel/50 hover:border-pixel/60 active:border-pixel/75 text-pixel border px-2 py-1 text-sm"
-                >
-                    Edit Profile
-                </a>
-            </div>
-            <div class="[&_a]:text-pixel mt-8 [&_a]:hover:underline">
-                <p>{{ $profile->bio  }}</p>
-            </div>
-            <dl class="mt-6 flex gap-6">
-                <div class="flex gap-1.5">
-                    <dd>{{ $profile->following_count }}</dd>
-                    <dt class="text-pixel-light/60">Following</dt>
-                </div>
-                <div class="flex gap-1.5">
-                    <dd>{{ $profile->followers_count }}</dd>
-                    <dt class="text-pixel-light/60">Followers</dt>
-                </div>
-            </dl>
-        </header>
 
-        <!-- Navigation/tabs -->
-        <div class="mt-6 h-full">
-            <nav class="overflow-x-auto [scrollbar-width:none]">
-                <ul class="flex min-w-max justify-end gap-8 text-sm">
-                    <li><a href="#">Posts</a></li>
-                    <li>
-                        <a class="text-pixel-light/60 hover:text-pixel-light/80" href="#"
-                        >Replies</a
-                        >
-                    </li>
-                    <li>
-                        <a class="text-pixel-light/60 hover:text-pixel-light/80" href="#"
-                        >Highlights</a
-                        >
-                    </li>
-                    <li>
-                        <a class="text-pixel-light/60 hover:text-pixel-light/80" href="#"
-                        >Inspiration Streams</a
-                        >
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <x-profile.header :profile="$profile" />
 
         <!-- Feed -->
         <ol class="border-pixel-light/10 border-t pt-4">
             <!-- Feed item -->
             @foreach($posts as $item)
-                @ <x-post
+                <x-post
                     :post="$item->isRepost() && $item->content === null ? $item->repostOf : $item"
                     :show-engagement="true"
                     :show-replies="true"
